a544024db1e1e99970ce46c1eb1e77bb92a77a0c
techcoder2007 <techcoder444@gmail.com>
2025-11-23 16:49:16 +0500

    initialize biome formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { biomePlugin } from '@pbr1111/vite-plugin-biome';
+
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), biomePlugin()],
 });
