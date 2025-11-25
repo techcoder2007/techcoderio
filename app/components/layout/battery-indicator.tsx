01916ca3c9d11befbbcdb7d6bd4aed9c3bb4d6b0
techcoder2007 <techcoder444@gmail.com>
2025-11-25 17:41:47 +0500

    feat:added error page route

--- app/components/layout/battery-indicator.tsx
+++ app/components/layout/battery-indicator.tsx
@@ -1,62 +1,20 @@
 import { BatteryCharging } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useBatteryStatus } from '~/hooks/useBatteryStatus';
 import { Battery } from '~/icons';
-import type { NavigatorWithBattery } from '~/interfaces/battery-manager';
-
 
 function BatteryIndicator() {
-  const [batteryLevel, setBatteryLevel] = useState<number | null>(null);
-  const [isCharging, setIsCharging] = useState<boolean>(false);
+  const {  charging,  level } =  useBatteryStatus();
 
-  useEffect(() => {
-    const getBatteryInfo = async () => {
-      const navigatorWithBattery = navigator as NavigatorWithBattery;
-
-      if (navigatorWithBattery.getBattery) {
-        try {
-          const battery = await navigatorWithBattery.getBattery!();
-          setBatteryLevel(battery.level);
-          setIsCharging(battery.charging);
-
-          const handleLevelChange = () => {
-            setBatteryLevel(battery.level);
-          };
-
-          const handleChargingChange = () => {
-            setIsCharging(battery.charging);
-          };
-
-          battery.addEventListener('levelchange', handleLevelChange);
-          battery.addEventListener('chargingchange', handleChargingChange);
-
-          // Cleanup function to remove event listeners
-          return () => {
-            battery.removeEventListener('levelchange', handleLevelChange);
-            battery.removeEventListener('chargingchange', handleChargingChange);
-          };
-        } catch (error) {
-          console.error('Battery status is not supported or an error occurred:', error);
-          setBatteryLevel(null);
-        }
-      } else {
-        console.warn('Battery Status API is not supported in this browser.');
-        setBatteryLevel(null);
-      }
-    };
-
-    getBatteryInfo();
-  }, []);
-
-  if (batteryLevel === null) {
+  if (level === null) {
     return <p>Battery information is not available.</p>;
   }
 
   return (
     <div className='flex items-center gap-1'>
       <BatteryStatus
-        isCharging={isCharging}
+        isCharging={charging}
       />
-      <p>{Math.round(batteryLevel * 100)}%</p>
+      <p>{Math.round(level * 100)}%</p>
     </div>
   );
 }
