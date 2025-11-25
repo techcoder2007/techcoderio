9fdfbdd87a61d13f46ace8d16af2a89c986f533d
techcoder2007 <techcoder444@gmail.com>
2025-11-26 00:05:21 +0500

    feat added github bug reporttemplate

--- app/views/welcome/index.tsx
+++ app/views/welcome/index.tsx
@@ -1,11 +1,9 @@
-import Activities from "~/components/layout/activities";
 import { Navbar } from "~/components/navbar";
 
 export default function Welcome() {
 	return (
 		<div className="min-h-screen bg-cover bg-center bg-no-repeat techcoderio-welcome">
 			<Navbar />
-			<Activities/>
 		</div>
 	); 
 }
