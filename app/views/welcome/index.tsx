8302ff3baba41803d39528c87d51bce9dc7ed722
techcoder2007 <techcoder444@gmail.com>
2025-11-25 19:04:28 +0500

    style design core component context menu

--- app/views/welcome/index.tsx
+++ app/views/welcome/index.tsx
@@ -1,11 +1,9 @@
-import { Spotify } from "~/apps";
 import { Navbar } from "~/components/navbar";
 
 export default function Welcome() {
 	return (
 		<div className="min-h-screen bg-cover bg-center bg-no-repeat techcoderio-welcome">
 			<Navbar />
-			<Spotify />
 		</div>
 	);
 }
